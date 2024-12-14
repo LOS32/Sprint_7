bebdd43a03d49d467e9a08150d3570510b0a2838
Dmitrii Losunov <dmitrij.dmitriy32@yandex.ru>
2024-12-13 20:26:53 -0800

    Добавлен тест на удаление курьера

---
+++ tests/courier/test_successful_delete_courier.py
@@ -0,0 +1,28 @@
+import pytest
+import allure
+from methods.courier_methods import CourierMethods
+from helpers import register_new_courier_and_return_login_password
+
+
+@allure.feature('Удаление курьера')
+class TestDeleteCourier:
+    @allure.title('Успешное удаление курьера')
+    def test_successful_delete_courier(self):
+        courier_methods = CourierMethods()
+        courier_data = register_new_courier_and_return_login_password()
+        courier_methods.create_courier(
+            courier_data[0],  # login
+            courier_data[1],  # password
+            courier_data[2]  # firstName
+        )
+        login_response = courier_methods.login_courier(
+            courier_data[0],  # login
+            courier_data[1]  # password
+        )
+        courier_id = login_response.json().get("id")
+        delete_response = courier_methods.delete_courier(courier_id)
+        assert delete_response.status_code == 200 and delete_response.json().get("ok") is True, (
+            f"Expected status code 200 and 'ok' to be True, but got "
+            f"status {delete_response.status_code} and response {delete_response.json()}."
+        )
+
