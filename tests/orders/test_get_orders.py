a7ea42b19be1481f3a70e875e0b123302fbd8855
Dmitrii Losunov <dmitrij.dmitriy32@yandex.ru>
2024-12-14 19:53:30 -0800

    Исправлены тесты на получение заказа по номеру

--- tests/orders/test_get_orders.py
+++ tests/orders/test_get_orders.py
@@ -1,4 +1,3 @@
-import pytest
 import allure
 from conftest import order
 
@@ -6,14 +5,9 @@ from conftest import order
 class TestGetOrders:
     @allure.title('Тест на проверку получения списка заказов')
     def test_get_orders(self, order):
-        # Создаём заказ через фикстуру `order`
         order["order_methods"].create_order(order["order_track"])
-
-        # Получаем список заказов
         response = order["order_methods"].get_orders()
         response_json = response.json()
-
-        # Проверяем, что ключ 'orders' есть в ответе и является списком
         assert isinstance(response_json.get("orders"), list), f"'orders' is not a list or missing: {response_json}"
 
 
