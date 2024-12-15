1131eedee6d9355d23318154f28fc97ee7e49d6e
Dmitrii Losunov <dmitrij.dmitriy32@yandex.ru>
2024-12-14 21:23:09 -0800

    Исправлен тест test_accept_order_invalid_courier_id

--- tests/orders/test_accept_order_invalid_courier_id.py
+++ tests/orders/test_accept_order_invalid_courier_id.py
@@ -4,9 +4,9 @@ from methods.courier_methods import CourierMethods
 from config import COURIER_DATA, ORDER_DATA, INVALID_COURIER_ID
 
 @allure.feature("Принятие заказа")
-class TestAcceptOrderMissingCourierId:
+class TestAcceptOrderInvalidCourierId:
     @allure.title("Попытка принять заказ без ID курьера")
-    def test_accept_order_missing_courier_id(self):
+    def test_accept_order_invalid_courier_id(self):
         order_methods = OrderMethods()
         courier_methods = CourierMethods()
         courier_methods.create_courier(
@@ -27,6 +27,6 @@ class TestAcceptOrderMissingCourierId:
         accept_order_response = order_methods.accept_order(order_id, invalid_courier_id)
         response_json = accept_order_response.json()
         assert accept_order_response.status_code == 404 and response_json.get(
-            "message") == "Заказа с таким id не существует", (
-            f"Expected status 404 with 'Заказа с таким id не существует', but got {accept_order_response.status_code} and {response_json}"
+            "message") == "Курьера с таким id не существует", (
+            f"Expected status 404 with 'Курьера с таким id не существует', but got {accept_order_response.status_code} and {response_json}"
         )
