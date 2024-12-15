b1b3363da00178487e3ff72c3542a2c2d2c06c4e
Dmitrii Losunov <dmitrij.dmitriy32@yandex.ru>
2024-12-14 21:57:40 -0800

    Исправлен тест test_accept_order_missing_courier_id

--- tests/orders/test_accept_order_missing_courier_id.py
+++ tests/orders/test_accept_order_missing_courier_id.py
@@ -1,7 +1,8 @@
 import allure
 from methods.order_methods import OrderMethods
 from methods.courier_methods import CourierMethods
-from config import COURIER_DATA, ORDER_DATA
+from config import COURIER_DATA, ORDER_DATA, NO_COURIER_ID
+
 
 @allure.feature("Принятие заказа")
 class TestAcceptOrderMissingCourierId:
@@ -19,15 +20,18 @@ class TestAcceptOrderMissingCourierId:
             COURIER_DATA["valid_courier"]["password"],
         )
         login_response.json().get("id")
+        no_courier_id = NO_COURIER_ID
         order_data = ORDER_DATA["order_data_black"]
         create_order_response = order_methods.create_order(order_data)
         track_number = create_order_response.json().get("track")
         get_order_response = order_methods.get_order_by_track(track_number)
         order_id = get_order_response.json()["order"].get("id")
-        accept_order_response = order_methods.accept_order(order_id, "")
+        accept_order_response = order_methods.accept_order(order_id, no_courier_id)  # Здесь не передаётся ID курьера
         response_json = accept_order_response.json()
-        assert accept_order_response.status_code == 400 and response_json.get("message") == "Недостаточно данных для поиска", (
+        assert accept_order_response.status_code == 400 and response_json.get(
+            "message") == "Недостаточно данных для поиска", (
             f"Expected status 400 with 'Недостаточно данных для поиска', but got {accept_order_response.status_code} and {response_json}"
         )
+
 
 
